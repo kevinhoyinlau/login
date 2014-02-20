fdfea0ab7a45d9b6c26dfb8f5e97904391551d6a
kevinhoyinlau <kevinhoyinlau@gmail.com>
2014-02-19 16:40:03 -0800

    Changed base url to home directory

--- login/urls.py
+++ login/urls.py
@@ -9,5 +9,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     #url(r'^admin/', include(admin.site.urls)),
-    url(r'^logon/', include('users.urls', namespace = 'users')),
+    url(r'^/?$', include('users.urls', namespace = 'users')),
 )
